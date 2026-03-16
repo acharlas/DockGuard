41c4a880ca3786e25ac69483cac81c101f7e4e4e
acharlas <acharlas@student.42.fr>
2026-03-16 23:04:16 +0100

    feat: add OCI and Cloudflare terraform providers with cloud backend

---
+++ terraform/provider.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.7"
+
+  cloud {
+    organization = "dockguard"
+    workspaces {
+      name = "dockguard-prod"
+    }
+  }
+
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 6.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "oci" {
+  tenancy_ocid     = var.oci_tenancy_ocid
+  user_ocid        = var.oci_user_ocid
+  fingerprint      = var.oci_fingerprint
+  private_key_path = var.oci_private_key_path
+  region           = var.oci_region
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
